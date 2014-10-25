67738b46e54cbeb6fd0cd9349c8c1989728d9a81
Kunal Mehta <legoktm@gmail.com>
2014-10-24 22:20:10 -0700

    Don't die when an error is found, go through all the other files too

--- checker.py
+++ checker.py
@@ -5,28 +5,40 @@ import os
 import sys
 
 
+class DuplicateKeyException(Exception):
+    def __init__(self, key):
+        self.key = key
+
+
 def checker(seq):
     d = {}
     for key, value in seq:
         if key in d:
-            raise ValueError("Duplicate key %r found" % key)
+            raise DuplicateKeyException(key)
         else:
             d[key] = value
     return d
 
 
 def check_directory(directory):
+    has_errors = False
     files = os.listdir(directory)
     for fname in files:
+        if fname.startswith('.'):
+            continue
         fname = os.path.join(directory, fname)
-        if os.path.isdir(fname) and not fname.startswith('.'):
-            check_directory(fname)
+        if os.path.isdir(fname):
+            has_errors = check_directory(fname) or has_errors
             continue
         if not (os.path.isfile(fname) and fname.endswith('.json')):
             continue
         print 'Checking %s...' % fname
-        check_file(fname)
-    return True
+        try:
+            check_file(fname)
+        except DuplicateKeyException, e:
+            print 'ERROR: Duplicate key found for "%s"' % e.key
+            has_errors = True
+    return has_errors
 
 
 def check_file(fname):
@@ -36,15 +48,22 @@ def check_file(fname):
     with open(fname) as f:
         text = f.read()
     json.loads(text, object_pairs_hook=checker)
-    # No exception was raised
-    return True
+    # No errors!
+    return False
 
 
 def main():
     fname = sys.argv[1]
-    if os.path.isdir(fname) and check_directory(fname):
-        print 'No duplicate keys found'
-    elif check_file(fname):
+    if os.path.isdir(fname):
+        if check_directory(fname):
+            # Errors found
+            print 'Duplicate keys found.'
+            sys.exit(1)
+        else:
+            print 'No duplicate keys found'
+            sys.exit(0)
+    else:
+        check_file(fname)
         print 'No duplicate keys found'
 
 if __name__ == '__main__':
--- checker_tests.py
+++ checker_tests.py
@@ -6,9 +6,15 @@ import checker
 
 
 class CheckerTest(unittest.TestCase):
-    def test_check_file(self):
-        self.assertTrue(checker.check_file('good.json'))
-        self.assertRaises(ValueError, checker.check_file, 'bad.json')
+    def test_check_file_good(self):
+        self.assertFalse(checker.check_file('good.json'))
+
+    def test_check_file_bad(self):
+        self.assertRaises(
+            checker.DuplicateKeyException,
+            checker.check_file,
+            'bad.json'
+        )
 
 if __name__ == '__main__':
     unittest.main()
